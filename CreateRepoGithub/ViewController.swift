f5fa8c7d7b0514995a846c2b9b9ab775dd55df8c
rajni.b@krescentglobal.com <tech.krescentglobal@gmail.com>
2018-09-25 18:44:31 +0530

    second commit

--- CreateRepoGithub/ViewController.swift
+++ CreateRepoGithub/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button:UIButton = UIButton(frame: CGRectMake(100, 400, 100, 50))
+        button.backgroundColor = UIColor.blackColor()
+        button.setTitle("Button", forState: UIControlState.Normal)
+        //button.addTarget(self, action:#selector(self.buttonClicked), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+        
+        func buttonClicked() {
+            print("Button Clicked")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
